b61084bc1e3cded8abb3852bfb45b03a4f4b5333
jacobfm <jacob_meidell@msn.com>
2014-10-17 14:28:12 +1100

    Initial commit

---
+++ src/com/example/Banter/BanterActivity.java
@@ -0,0 +1,15 @@
+package com.example.Banter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BanterActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
