89a856b4e767829d925a300f47d88fcaf53a3b20
jacobfm <jacob_meidell@msn.com>
2014-10-17 15:48:57 +1100

    Created custom action bar for menu fragment.

--- src/com/example/Banter/BanterActivity.java
+++ src/com/example/Banter/BanterActivity.java
@@ -2,6 +2,8 @@ package com.example.Banter;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class BanterActivity extends Activity {
     /**
@@ -11,5 +13,19 @@ public class BanterActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bantermenulayout);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        super.onCreateOptionsMenu(menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem menuItem){
+        switch(menuItem.getItemId()){
+            case R.id.add:
+                break;
+            case R.id.search:
+                break;
+        }
+        return super.onOptionsItemSelected(menuItem);
     }
 }
--- src/com/example/Banter/BanterMenuFragment.java
+++ src/com/example/Banter/BanterMenuFragment.java
@@ -1,9 +1,30 @@
 package com.example.Banter;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.*;
 
 /**
  * Created by jacobmeidell on 17.10.14.
  */
 public class BanterMenuFragment extends Fragment {
+
+    View banterMenuFragment;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        getActivity().setContentView(banterMenuFragment);
+        setHasOptionsMenu(true);
+        banterMenuFragment = inflater.inflate(R.layout.bantermenulayout,container,false);
+
+        return banterMenuFragment;
+    }
+
+    /* Set the custom action bar to the menu fragment */
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
+        super.onCreateOptionsMenu(menu,inflater);
+        menu.clear();
+        inflater.inflate(R.menu.bantermenuactionbar,menu);
+    }
 }
