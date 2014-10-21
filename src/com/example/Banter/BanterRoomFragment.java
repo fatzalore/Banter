634df875bc0385b729792f584483743433fd51ef
Erlend Vie <erlend_t_vie@msn.com>
2014-10-21 13:54:47 +1100

    fixed posts bugg and polling error

--- src/com/example/Banter/BanterRoomFragment.java
+++ src/com/example/Banter/BanterRoomFragment.java
@@ -217,28 +217,30 @@ public class BanterRoomFragment extends Fragment {
 
             /* what is id of last post? if exists */
             if (current.getPosts().size() > 0) {
-                params.add(new BasicNameValuePair(BanterSQLContract.TAG_POST_ID,Integer.toString(current.getPosts().get(current.getPosts().size()-1).getId())));
+                params.add(new BasicNameValuePair(BanterSQLContract.TAG_POST_ID,Integer.toString(current.getPosts().get(0).getId())));
+                Log.e("POST POLLING", "asking for post with name : " + current.getPosts().get(0).getName());
             } else {
                 params.add(new BasicNameValuePair(BanterSQLContract.TAG_POST_ID,Integer.toString(0)));
+                Log.e("POST POLLING", "asking for post with id : " + 0);
             }
 
             JSONObject json = jsonParser.makeHttpRequest(BanterSQLContract.URL_GET_POSTS,"GET",params);
             try{
                 int success = json.getInt(BanterSQLContract.TAG_SUCCESS);
-                Log.v("POST POLLING", "new post request to DB : " + json.getString(BanterSQLContract.TAG_SUCCESS));
+                Log.e("POST POLLING", "new post request to DB : " + json.getString(BanterSQLContract.TAG_SUCCESS));
                 if (success == 1) {
                     posts = json.getJSONArray(BanterSQLContract.TAG_POSTS);
                     for (int i = 0; i < posts.length(); i++) {
                         JSONObject c = posts.getJSONObject(i);
 
                         BanterPost banterPost = new BanterPost();
-                        banterPost.setId(c.getInt(BanterSQLContract.TAG_POST_ID));
+                        banterPost.setId(c.getInt(BanterSQLContract.TAG_ID));
                         banterPost.setName(c.getString(BanterSQLContract.TAG_NAME));
                         banterPost.setLikes(c.getInt(BanterSQLContract.TAG_LIKES));
                         banterPost.setText(c.getString(BanterSQLContract.TAG_TEXT));
                         banterPost.setTime(c.getString(BanterSQLContract.TAG_TIME));
 
-                        banterActivity.getBanterDataModel().currentRoom.addPost(banterPost);
+                        banterActivity.getBanterDataModel().currentRoom.getPosts().add(0, banterPost);
                     }
                 }
             } catch (Exception e) {
@@ -248,12 +250,14 @@ public class BanterRoomFragment extends Fragment {
         }
         @Override
         protected void onPostExecute(String file_url) {
-            getActivity().runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    getBanterRoomListAdapter().notifyDataSetChanged();
-                }
-            });
+            if (timer != null) {
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        getBanterRoomListAdapter().notifyDataSetChanged();
+                    }
+                });
+            }
         }
     }
 
@@ -282,7 +286,7 @@ public class BanterRoomFragment extends Fragment {
             try{
                 int success = jsonObject.getInt(BanterSQLContract.TAG_SUCCESS);
                 if(success == 1){
-                    banterActivity.banterDataModel.currentRoom.addPost(currentPost);
+                    //banterActivity.banterDataModel.currentRoom.getPosts().add(0, currentPost);
                 }
             } catch (Exception e){
                 e.printStackTrace();
@@ -312,7 +316,7 @@ public class BanterRoomFragment extends Fragment {
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
-                Log.v("POST POLLING", "TRYING TO GET NEW POSTS");
+                Log.e("POST POLLING", "TRYING TO GET NEW POSTS");
                 new PostPolling().execute();
             }
         }, 0, 5000);
@@ -323,5 +327,7 @@ public class BanterRoomFragment extends Fragment {
         super.onPause();
         /* cancel post polling when user is about to leave fragment */
         timer.cancel();
+        /* timer set to null so background task knows if we have left the fragment */
+        timer = null;
     }
 }
